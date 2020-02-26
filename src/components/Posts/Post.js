3de8bbbef33fdfb6474a4bd5bd72a987d9d4accf
Heidi Boulter <boulter.heidi@gmail.com>
2020-02-25 18:37:12 -0600

    add buttons to posts

--- src/components/Posts/Post.js
+++ src/components/Posts/Post.js
@@ -1,6 +1,13 @@
 import React from "react";
 import Comment from "./Comment";
 import "./Post.css";
+import {
+  faHeart,
+  faComment,
+  faInfoCircle
+} from "@fortawesome/free-solid-svg-icons";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Post = ({ image }) => {
   return (
@@ -10,6 +17,16 @@ const Post = ({ image }) => {
         <span className="profile__name">h_dang</span>
       </span>
       <img src={image.src} alt="uh oh!" width="100%" className="image" />
+      <div className="buttons">
+        <span className="buttons__left">
+          <FontAwesomeIcon icon={faHeart} className="heart" />
+          <FontAwesomeIcon icon={faComment} className="comment" />
+          <FontAwesomeIcon icon={faInfoCircle} className="info" />
+        </span>
+        <span className="buttons__right">
+          <FontAwesomeIcon icon={faGithub} className="github" />
+        </span>
+      </div>
       <h4>{image.likes}</h4>
       <p>
         <b>h_dang</b> {image.description}
